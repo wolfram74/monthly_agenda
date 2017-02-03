ca7ba35ed6fb8fd3a121367b302b5157b2715937
wolfram74 <phaugen@gmail.com>
2017-02-03 12:39:48 -0600

    rough sketch of data structure

---
+++ docs/scripts/main.js
@@ -0,0 +1,54 @@
+function viewInitialize(){
+  if(typeof state.checkInDay === 'undefined'){
+    // do checkInDay prompt
+    return
+  }
+  if(typeof state[currentMonth()]==='undefined'){
+    // do set goals prompt
+    return
+  }
+}
+
+function currentMonth(){
+  const today = new Date()
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  return `y${year}m${month}`
+}
+
+function saveState(){
+  localStorage.setItem('state', JSON.stringify(state));
+}
+
+function loadState(){
+  return JSON.parse(localStorage.getItem('state')) || {};
+}
+
+console.log('farts')
+const state = loadState()
+const weekDays = [
+  'Sunday', 'Monday', 'Tuesday', 'Wednesday',
+  'Thursday', 'Friday', 'Saturday']
+const months = [
+  'January', 'February', 'March', 'April',
+  'May', 'June', 'July', 'August',
+  'September', 'October', 'November', 'December']
+
+var demoState = {
+  'y2017m2': {
+    'weeks':[
+      {'dailyProgress':[
+        [false, false],
+        [false, false],
+        [false, false],
+        [true, false],
+        [false, false],
+        [false, false],
+        [false, false],
+        ],
+      'weeklyReflection':['minimal progress', 'no progress']
+      },
+    ],
+    'goals':['guitar', 'programming']
+  }
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,23 @@
 #Features
-*setting a check in day
-*setting goals on last check in day of the month for next month
-*auto generating status update form for each check in
+
+* setting a check in day
+* setting goals on last check in day of the month for next month
+* auto generating status update form for each check in
+
+#Possible data format
+top level data
+checkInDay:int
+yearMonth: month object
+monthObject
+  goals: array of goal objects
+  weeks: array of week objects
+  monthlyReflection: array of reflection strings
+  monthlyProgress: array of 0-2 progress indicator
+weekObject
+  dailyChecks: array of arrays with booleans
+  weeklyProgress: array of 0-2 progress indicators
+  weeklyReflection: array of reflection strings
+goalObject
+  description: string describing broad goal
+  plan: how goal will be met
+  nonZeroThreshold: int describing minimum minutes for "worked on"
