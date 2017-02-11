077bf9a11b35cb2fe05d920467c29d3570300596
wolfram74 <phaugen@gmail.com>
2017-02-11 12:46:34 -0600

    added more tests, incomplete solution

--- docs/jasmine-standalone-2.2.0/spec/SourceSpec.js
+++ docs/jasmine-standalone-2.2.0/spec/SourceSpec.js
@@ -10,11 +10,25 @@ describe("Meta", function() {
 
 describe('utils module', function(){
   describe('#nthWeek', function(){
+    it('if checkin is also the first day of the month, first day of the month is in week 0', function(){
+      let day1 = new Date(2017,1,1);
+      let day2 = new Date(2017,3,1);
+      expect(utils.nthWeek(day1, 3)).toEqual(0)
+      expect(utils.nthWeek(day2, 6)).toEqual(0)
+    })
+    it('if checkin is also the first day of the month, 2nd day of the month is in week 1', function(){
+      let day1 = new Date(2017,1,2);
+      let day2 = new Date(2017,4,2);
+      expect(utils.nthWeek(day1, 3)).toEqual(1)
+      expect(utils.nthWeek(day2, 1)).toEqual(1)
+    })
     it('should identify days before the first threshold', function(){
       let day1 = new Date(2017, 1, 1);
       let day2 = new Date(2017, 1, 3);
       let day3 = new Date(2017, 6, 1);
+      let day4 = new Date(2017, 1, 2);
       expect(utils.nthWeek(day1, 0)).toEqual(0)
+      expect(utils.nthWeek(day1, 3)).toEqual(1)
       expect(utils.nthWeek(day2, 6)).toEqual(0)
       expect(utils.nthWeek(day3, 1)).toEqual(0)
     })
--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -37,30 +37,41 @@ utils = (function(){
   };
 
   API.nthWeek = function(dateObj, checkIn){
+    /*
+    day, shift, checkin -> nth week
+    1, s, s+0 -> 0
+    d, s, s+0 -> ceil(((d-1)/7)
+    (1, 2), s, s+1 -> 0
+    d, s, s+1 ->
+    */
     const year = dateObj.getFullYear();
     const month = dateObj.getMonth();
     const monthLength = new Date(year, month+1, 0).getDate();
     const weekShift = new Date(year, month, 1).getDay();
     const today = dateObj.getDate();
-    let week = Math.floor((today+weekShift-checkIn)/7);
-    console.log(month, today, monthLength, week, week*7 , weekShift)
+    let week = Math.abs(
+      Math.ceil(
+        (today-1-(weekShift-checkIn))/7
+        )
+      );
+    // console.log(month, today, monthLength, week, checkIn , weekShift)
     // return monthLength < (week+1)*7-weekShift ? week : 0;
     return week
-    // if(today+weekShift -7< checkIn){
-    //   return 0
-    // }
-    // if(today+weekShift -14< checkIn){
-    //   return 1
-    // }
-    // if(today+weekShift -21< checkIn){
-    //   return 2
-    // }
-    // if(today+weekShift -28< checkIn){
-    //   return 3
-    // }
-    // if(today+weekShift -35< checkIn){
-    //   return 4
-    // }
+    if(today+weekShift -7< checkIn){
+      return 0
+    }
+    if(today+weekShift -14< checkIn){
+      return 1
+    }
+    if(today+weekShift -21< checkIn){
+      return 2
+    }
+    if(today+weekShift -28< checkIn){
+      return 3
+    }
+    if(today+weekShift -35< checkIn){
+      return 4
+    }
     // return Math.floor((today+weekShift-checkIn)/7)
   };
 
