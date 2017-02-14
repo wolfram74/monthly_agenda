6d69e9271a987286184ebea9e1f1dd8c588bf5a6
wolfram74 <phaugen@gmail.com>
2017-02-14 16:34:46 -0600

    cleared out trial solution

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -37,56 +37,7 @@ utils = (function(){
   };
 
   API.nthWeek = function(dateObj, checkIn){
-    /*
-    day, shift, checkin -> nth week
-    1, s, s+0 -> 0
-    d, s, s+0 -> ceil(((d-1)/7)
-    (1, 2), s, s+1 -> 0
-    d, s, s+1 ->
-    */
-    const year = dateObj.getFullYear();
-    const month = dateObj.getMonth();
-    const monthLength = new Date(year, month+1, 0).getDate();
-    const weekShift = new Date(year, month, 1).getDay();
-    const dayOfWeek = dateObj.getDay()
-    // const today = dateObj.getDate();
-    const today = dateObj.getDate()-1;
-    const fullWeeks = Math.floor(today/7);
-    const weekRemainder = today % 7;
-    let week = Math.abs(
-      Math.ceil(
-        (today-1-(weekShift-checkIn))/7
-        )
-      );
-    // const gap = (weekShift-checkIn+7+1)%7;
-    const gap2 = (checkIn - weekShift)%7 ;
-    // const bonus = (dayOfWeek > gap) ? 1 : 0;
-    const bonus2 = (dayOfWeek > gap2) ? 1 : 0;
-    // console.log(month, today, ['dw', dayOfWeek], ['r',weekRemainder], ['c', checkIn], ['s',weekShift], bonus)
-    // return Math.abs(Math.ceil((today-checkIn)/7))
-    // return Math.abs(Math.ceil((today-gap)/7))
-    // return Math.floor((today-gap)/7+1)
-    // return fullWeeks + bonus
-    return fullWeeks + bonus2
-    // console.log(month, today, monthLength, week, checkIn , weekShift)
-    // return monthLength < (week+1)*7-weekShift ? week : 0;
-    // return week
-    // if(today+weekShift -7< checkIn){
-    //   return 0
-    // }
-    // if(today+weekShift -14< checkIn){
-    //   return 1
-    // }
-    // if(today+weekShift -21< checkIn){
-    //   return 2
-    // }
-    // if(today+weekShift -28< checkIn){
-    //   return 3
-    // }
-    // if(today+weekShift -35< checkIn){
-    //   return 4
-    // }
-    // return Math.floor((today+weekShift-checkIn)/7)
+
   };
 
   API.checkInDays = function(){
